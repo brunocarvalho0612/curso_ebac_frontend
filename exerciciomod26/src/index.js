a34ab0e380826df0d60be6b5501d9b8ed2a3dd48
bruno <brunocarvalhorodrigues@hotmail.com>
2026-01-02 08:26:45 -0300

    Solucionado erro de nao subir os arquivos do exercicio

---
+++ exerciciomod26/src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicar(6, 7));
+console.log(saudacao("Bruno"));
---
+++ exerciciomod26/src/index.ts
@@ -0,0 +1,13 @@
+
+function multiplicar(a: number, b: number): number {
+  return a * b;
+}
+
+
+function saudacao(nome: string): string {
+  return "Olá " + nome;
+}
+
+
+console.log(multiplicar(6, 7));
+console.log(saudacao("Bruno"));
